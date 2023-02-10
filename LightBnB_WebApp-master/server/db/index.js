87523e6e0f639caa54bd2a21b8008b026c39df5a
andrii-grynik <andrii.grynik@gmail.com>
2023-02-09 17:02:20 -0700

    In case I forgot something, pushing whole directory

---
+++ LightBnB_WebApp-master/server/db/index.js
@@ -0,0 +1,24 @@
+
+// Connect to db lighbnb
+const pg = require('pg');
+const Pool = pg.Pool;
+
+const options = {
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb',
+  port: '5432'
+}
+
+const pool = new Pool(options);
+
+pool.connect(() => {
+  console.log('connected to db!');
+});
+
+module.exports = {
+    query: (text, params, callback) => {
+      return pool.query(text, params, callback)
+    },
+}
